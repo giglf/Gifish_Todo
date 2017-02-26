c0484e5bdd2ee130db734a2d2e6956f494f52e3e
giglf <lin.giglf@gmail.com>
2017-02-26 15:43:06 +0800

    Finish AboutActivity.class

--- app/src/main/java/tk/gifish/gifish_todo/activity/AboutActivity.java
+++ app/src/main/java/tk/gifish/gifish_todo/activity/AboutActivity.java
@@ -1,15 +1,76 @@
 package tk.gifish.gifish_todo.activity;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import tk.gifish.gifish_todo.R;
 
 public class AboutActivity extends AppCompatActivity {
 
+    private TextView versionTextView;
+    private String appVersion = "0.1";
+    private Toolbar toolbar;
+    private TextView contactMe;
+    String theme;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        theme = getSharedPreferences(MainActivity.THEME_PREFERENCES, MODE_PRIVATE).getString(MainActivity.THEME_SAVED, MainActivity.LIGHTTHEME);
+        if(theme.equals(MainActivity.DARKTHEME)){
+            setTheme(R.style.CustomStyle_DarkTheme);
+        } else{
+            setTheme(R.style.CustomStyle_LightTheme);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_layout);
+
+        Intent i = getIntent();
+
+        final Drawable backArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        if(backArrow!=null){
+            backArrow.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+        }
+
+        try{
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), 0);
+            appVersion = info.versionName;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        versionTextView = (TextView)findViewById(R.id.aboutVersionTextView);
+        versionTextView.setText(String.format(getResources().getString(R.string.app_version), appVersion));
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(backArrow);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:
+                if(NavUtils.getParentActivityName(this)!=null){
+                    NavUtils.navigateUpFromSameTask(this);
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
